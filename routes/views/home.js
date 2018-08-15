6a34b5034a54d90730f2c2152227e257c179afa0
zhaoyake <zhaoyake@dudu84.com>
2018-08-15 16:53:44 +0800

    修改两种异步方式

--- routes/views/home.js
+++ routes/views/home.js
@@ -2,41 +2,42 @@
  *  @name route-views-home
  * */
 const request = require('../../core/request');
+const View = require('../../core/views')
 const routeList = [];
 routeList.push({
     method: 'get',
-    path: '/ee',
-    handler: async function(ctx, next) {
+    path: '/',
+    handler: async function (ctx, next) {
         ctx.locals.section = 'home';
         ctx.locals.data = {
             banner_list: [],
             news_list: [],
         };
-        await request.post('/index.php/activities/banner', {
-            city: '深圳市',
-            show_type: 9,
-        }, async function (result) {
-            ctx.locals.data.banner_list = result.data;
-            await next();
+        const view = new View(ctx, next);
+        view.on('init', function (next) {
+            request.post('/index.php/activities/banner', {
+                city: '深圳市',
+                show_type: 9,
+            }, function (result) {
+                view.ctx.locals.data.banner_list = result.data;
+                next();
+            });
         });
-    },
-});
-routeList.push({
-    method: 'get',
-    path: '/reee',
-    handler: async function(ctx, next) {
-        request.post('/index.php/official/news/get_list', {
-            page: 1,
-        }, function (result) {
-            ctx.locals.data.news_list = result.data.list;
-            next();
+        view.on('init', function (next) {
+            request.post('/index.php/official/news/get_list', {
+                page: 1,
+            }, function (result) {
+                view.ctx.locals.data.news_list = result.data.list;
+                next();
+            });
         });
+        await view.render('index');
     },
 });
 
 routeList.push({
     method: 'get',
-    path: '/',
+    path: '/333',
     handler: [
         async function(ctx, next) {
             ctx.locals.section = 'home';
