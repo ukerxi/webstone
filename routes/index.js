250d852f02f716679deaf8680dbe2b2d3b722631
zhaoyake <zhaoyake@dudu84.com>
2018-11-12 18:35:57 +0800

    修改路由页面进行扩展，公用模块抽取

--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,18 @@
  *  @description register page route
  * */
 const _ = require('lodash');
-const path = require('path');
+const common = require('./../core/common.js');
 let routeList = [];
 
 // 注册路由
 // home page
-registerRoute('/', require(getPath('home')));
+_.forEach(common.registerRoute('/', 'home'), function(item) {
+    routeList.push(item);
+});
 // template page
-registerRoute('/post', require(getPath('template')));
-
-
-function registerRoute(path, handler) {
-    // handle routes
-    if (_.isFunction(handler)) {
-        routeList.push({
-            path: '/',
-            handler: handler,
-        });
-    } else if (_.isArray(handler)) {
-        _.forEach(handler, function(item) {
-            routeList.push({
-                path: item.path === '/' ? path : path + item.path,
-                handler: item.handler,
-                method: item.method,
-            });
-        });
-    }
-}
-
-function getPath(str) {
-    // 直接定位到 views 文件夹下
-    return path.join(__dirname, './views/', str);
-}
-
+_.forEach(common.registerRoute('/post', 'template'), function(item) {
+    routeList.push(item);
+});
 
 // 导出模块
 module.exports = {
--- routes/views/template.js
+++ routes/views/template.js
@@ -1,7 +1,7 @@
 /**
  *  @name route-views-template
  * */
-const path = require('path');
+const request = require('../../core/request');
 const routeList = [];
 routeList.push({
     method: 'get',
@@ -22,6 +22,40 @@ routeList.push({
         });
         next();
     },
+});
+
+routeList.push({
+    method: 'get',
+    path: '/333',
+    handler: [
+        async function(ctx, next) {
+            ctx.locals.section = 'home';
+            ctx.locals.data = {
+                banner_list: [],
+                news_list: [],
+            };
+            await request.post('/index.php/activities/banner', {
+                city: '深圳市',
+                show_type: 9,
+            }, async function (result) {
+                ctx.locals.data.banner_list = result.data;
+                await next();
+            }, {isPromise: true});
+        },
+        async function(ctx, next) {
+            await request.post('/index.php/official/news/get_list', {
+                page: 1,
+            }, async function (result) {
+                ctx.locals.data.news_list = result.data.list;
+                await next();
+            }, {isPromise: true});
+        },
+        async function(ctx, next) {
+            ctx.locals.test_trrr = 'sfsfsfsf';
+            await ctx.render('index', ctx.locals);
+            next();
+        },
+    ],
 });
 
 module.exports = routeList;
